add43e3343270fbce59b0d97c050af2eee09ac2a
kongkongqixi <kongkongqixi@163.com>
2019-05-15 16:16:00 +0800

    三个数之和为0的所有组合方式

--- 15_sum_of_three_squares.py
+++ 15_sum_of_three_squares.py
@@ -1,4 +1,66 @@
 class Solution:
+    def a(self, nums):
+        plus = []
+        neg = []
+        zero_len = 0
+        num_dict = {}
+        result = []
+        for i in nums:
+            if i in num_dict.keys():
+                num_dict[i] += 1
+            else:
+                num_dict[i] = 1
+
+            if i > 0 and i not in plus:
+                plus.append(i)
+            elif i < 0 and i not in neg:
+                neg.append(i)
+            elif i == 0:
+                zero_len += 1
+
+        if len(neg) == 0:
+            if zero_len < 3:
+                return []
+            else:
+                return [[0, 0, 0]]
+
+        if len(plus) == 0:
+            if zero_len < 3:
+                return []
+            else:
+                return [[0, 0, 0]]
+
+        neg.sort()
+        plus.sort()
+        if zero_len >= 3:
+            result.append([[0,0,0]])
+        for n in neg:
+            if (0-n/2 in plus) and (num_dict[0-n/2] >1):
+                result.append([n,int(0-n/2), int(0-n/2)])
+            for p in plus:
+                val = 0 - n - p
+                if val > 0:
+                    if val <= p:
+                        continue
+                    if val in plus:
+                        result.append([val, p, n])
+                elif val < 0 :
+                    if val < n:
+                        break
+                    elif val == n and num_dict[n] > 1:
+                        if val in neg:
+                            result.append([n, val, p])
+                    elif val >n and val in neg:
+                        result.append([n, val, p])
+
+                elif val == 0:
+                    if zero_len > 0:
+                        result.append([n, p, 0])
+        return result
+
+
+
+
     def threeSum2(self, nums):
         nums.sort()
         result = []
@@ -53,4 +115,4 @@ class Solution:
                 k -= 1
         return result
 S = Solution()
-print(S.threeSum1([[-1,0,1,2,-1,-4]]))
+print(S.a([[-1,0,1,2,-1,-4]]))
